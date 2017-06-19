2e17e90a479aec82bc2991c43cdc1a2d0bb611d6
iii <iii@userde-MacBook-6.local>
2017-06-19 10:11:48 +0800

    v1

---
+++ RongS10/RongAPI.swift
@@ -0,0 +1,29 @@
+//
+//  RongAPI.swift
+//  RongS10
+//
+//  Created by iii on 2017/6/19.
+//  Copyright © 2017年 iii. All rights reserved.
+//
+
+import Foundation
+
+class Rong01 {
+    var x = 1
+    func m1() {
+        print("Rong01:m1():x = \(x)")
+    }
+}
+class Rect {
+    var x:Int = 0
+    var y:Int = 0
+    var w:Int = 0
+    var h:Int = 0
+}
+
+struct Rong02 {
+    var x = 1
+    func m1() {
+        print("Rong01:m1():x = \(x)")
+    }
+}
--- RongS10/main.swift
+++ RongS10/main.swift
@@ -8,5 +8,72 @@
 
 import Foundation
 
-print("Hello, World!")
+// 屬性擴張
+extension Rong01 {
+    var y:Int { return x * 2 }
+}
+var Obj1 = Rong01()
+print(Obj1.y)
 
+extension Double {
+    var m: Double { return self}
+    var km: Double { return self / 1000.0 }
+    var cm: Double { return self * 100.0 }
+}
+var len = 123.0
+print(len.m)
+print(len.km)
+print(len.cm)
+var len2 = 100.0
+var len3 = 1.km + 4.cm
+print(len3.m)
+
+extension Rect {
+    var cx:Int { return x + w / 2}
+    var cy:Int { return y + h / 2}
+}
+extension Rong01 {
+    convenience init(x:Int){
+        self.init()
+        self.x = 100
+    }
+}
+var Obj2 = Rong01(x:4)
+print(Obj2.x)
+var Obj3 = Rong01()
+print(Obj3.x)
+
+extension Rong01 {
+    func m2()  {
+        x += 4
+        print("Rong01:m2()x = \(x)")
+    }
+}
+var Obj4 = Rong01()
+Obj4.m2()
+
+extension Rong02 {
+    mutating func m2() {
+        x += 4
+        print("Rong01:m2(): x = \(x)")
+    }
+}
+var Obj5 = Rong02()
+Obj5.m2()
+
+extension Int {
+    subscript(d:Int) -> Int {
+        var x1 = 1
+        for _ in 0...d {
+            x1 *= 10
+        }
+        var x2 = 1
+        for _ in 0..<d {
+            x2 *= 10
+        }
+        let x3 = self % x1
+        let ret = x3 / x2
+        return ret
+    }
+}
+print(123456789[5])
